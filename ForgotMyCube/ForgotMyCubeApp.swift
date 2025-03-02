9c9de6a99f0b412a03027dc13329c5fa93f32a52
Aniruddha Adhikary <932949+aniruddha-adhikary@users.noreply.github.com>
2025-03-02 11:24:08 +0800

    Initial Commit

---
+++ ForgotMyCube/ForgotMyCubeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ForgotMyCubeApp.swift
+//  ForgotMyCube
+//
+//  Created by Aniruddha Adhikary on 2/3/25.
+//
+
+import SwiftUI
+
+@main
+struct ForgotMyCubeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ForgotMyCubeTests/ForgotMyCubeTests.swift
@@ -0,0 +1,17 @@
+//
+//  ForgotMyCubeTests.swift
+//  ForgotMyCubeTests
+//
+//  Created by Aniruddha Adhikary on 2/3/25.
+//
+
+import Testing
+@testable import ForgotMyCube
+
+struct ForgotMyCubeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
